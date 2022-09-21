954d16703dec7885b910efbb1bae982961c80066
Arif Mazumder <abidmazumder2007@gmail.com>
2022-09-21 11:09:28 +0600

    change

--- src/utils/forcast.js
+++ src/utils/forcast.js
@@ -11,7 +11,9 @@ const forecast = (latitude, longitude, callback) => {
       callback(
         undefined,
         `${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature} degrees out feels like ${body.current.feelslike} out ,` +
-          `<br> UV Index: ${body.current.uv_index}`
+          `<br> UV Index: ${body.current.uv_index} <br>
+          Humidity:${body.current.humidity}
+          `
       );
     }
   });
